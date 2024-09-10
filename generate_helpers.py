e2797bab5e85decaaa299dabca22fb8fe6a69c07
Sinem <38989094+sinemmy@users.noreply.github.com>
2024-09-10 16:54:43 -0400

    wip generate_helpers
    adding functions to generate_helpers

--- generate_helpers.py
+++ generate_helpers.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from pathlib import Path
+from sae_lens import SAE, HookedSAETransformer
+
 
 def load_json_dataset(str_path:str):
   '''
@@ -12,8 +14,27 @@ def load_json_dataset(str_path:str):
   elif path.suffix == '.jsonl' :
       df = pd.read_json(str_path,lines=True)
   else: 
-      raise ValueError('Invalid file type') 
+      raise ValueError('Invalid file type: Currently handles .json and .jsonl')
   return df
 
 
+def load_SAEs (sae_release:str, sae_id_list:list, device) -> list:
+    '''
+        Instead of loading each SAE individually, this helps you load all the SAEs you want all at once and outputs a list of the SAEs 
+    '''
+    #initialize
+    n_saes = len(sae_list)
+    sae_list = [None]*n_saes
+    cfg_dict_list = [None]*n_saes
+    sparsity_list = [None]*n_saes
+
+
+    for i in range(n_saes):
+        sae_list[i], cfg_dict_list[i], sparsity_list[i] = \
+        SAE.from_pretrained(
+                    release = sae_release, # <- Release name 
+                    sae_id = sae_id_list[i], # <- SAE id (not always a hook point!)
+                    device = device
+                )
+    return sae_list, cfg_dict_list, sparsity_list
 
