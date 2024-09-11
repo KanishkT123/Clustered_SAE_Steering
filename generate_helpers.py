63e0328bc4bae0582649cc88739205f869ad6f87
Sinem <38989094+sinemmy@users.noreply.github.com>
2024-09-11 19:37:31 -0400

    Update generate_helpers.py
    committing so i dont lose

--- generate_helpers.py
+++ generate_helpers.py
@@ -20,7 +20,7 @@ def load_json_dataset(str_path:str):
       raise ValueError('Invalid file type: Currently handles .json and .jsonl')
   return df
 
-# LOAD MULTIPLE SAES and get a list
+# LOAD MULTIPLE SAES AND GET A LIST OF OUTPUTS
 def load_pretrained_SAEs (sae_release:str, sae_id_list:list, device) -> list:
     '''
     load_SAEs : Instead of loading each pretrained SAE individually, 
@@ -28,6 +28,7 @@ def load_pretrained_SAEs (sae_release:str, sae_id_list:list, device) -> list:
     and outputs a list of the SAEs, and corresponding lists
     with their cfg dicts and sparsity values (note: ok if sparsity values are None) 
     '''
+
     #initialize
     n_saes = len(sae_id_list)
     sae_list = [None]*n_saes
@@ -38,22 +39,24 @@ def load_pretrained_SAEs (sae_release:str, sae_id_list:list, device) -> list:
     for i in range(n_saes):
         sae_list[i], cfg_dict_list[i], sparsity_list[i] = \
         SAE.from_pretrained(
-                    release = sae_release, # <- Release name 
+                    release = sae_release, # <- Release name
                     sae_id = sae_id_list[i], # <- SAE id (not always a hook point!)
                     device = device
                 )
     return sae_list, cfg_dict_list, sparsity_list
 
 
+
 # LOAD SAE DIRECTORY AND FILTER BY MODEL AND RELEASES OF INTEREST
+# ADDED TO GENERATE_HELPERS.PY : LOAD SAE DIRECTORY AND FILTER BY MODEL AND RELEASES OF INTEREST
 def sae_directory_info(model=None, release=None, exact_match_model:bool=True, exact_match_release:bool=False)-> DataFrame:
     '''
     Returns a filtered version of the sae directory data frame - using get_pretrained_saes_directory of SAELens
     if model is None, then returns everything that  get_pretrained_saes_directory of SAELens would
     Model and Release can be keyword that should be in the name e.g. 'gemma-scope' or an exact name
-    By default, need to match the model exactly but not the release 
+    By default, need to match the model exactly but not the release
     '''
-    # directly from tutorial 2.0 of sae lens  
+    # directly from tutorial 2.0 of sae lens
     model_df = pd.DataFrame.from_records({k:v.__dict__ for k,v in get_pretrained_saes_directory().items()}).T
     model_df.drop(columns=["expected_var_explained", "expected_l0", "config_overrides", "conversion_func"], inplace=True)
 
@@ -66,7 +69,7 @@ def sae_directory_info(model=None, release=None, exact_match_model:bool=True, ex
         if exact_match_model:
             model_df = model_df[model_df['model']==model]
         else:
-            model_df= model_df[[model in r for r in model_df['model'].to_list()]]
+            model_df = model_df[[model in r for r in model_df['model'].to_list()]]
 
     # case release is not None
     if release is not None:
@@ -75,3 +78,5 @@ def sae_directory_info(model=None, release=None, exact_match_model:bool=True, ex
         else:
             model_df = model_df[[release in r for r in model_df['release'].to_list()]]
     return model_df
+
+
