c512cc591a247e5097c6cc03d86efcaa9d9a2262
Sinem <38989094+sinemmy@users.noreply.github.com>
2024-09-11 19:43:21 -0400

    generate_helepers: functions for easier sae selection
    Added functions to filter out dataframe results, so for gpt2 you can get the sae interest by: gpt_df = sae_directory_info(release='gpt2-small-res-jb', exact_match_release=True)
    gpt_ids = list(gpt_df.saes_map.to_list()[0].keys())
    get_saeids_for_layer(gpt_ids, layer=10)

--- generate_helpers.py
+++ generate_helpers.py
@@ -79,4 +79,75 @@ def sae_directory_info(model=None, release=None, exact_match_model:bool=True, ex
             model_df = model_df[[release in r for r in model_df['release'].to_list()]]
     return model_df
 
+######################################################################################################
+### FUNCTIONS TO FILTER OUT DATAFRAME RESULTS TO HELP LOAD APPROPRIATE SAES ####
+### GEMMA-SCOPE SPECIFIC FUNCTIONS UNLESS OTHERWISE SPECFIED !!!   
+######################################################################################################
+def get_saeids_for_layer(sae_id_list:list, layer:int=0, width:int=16):
+    '''
+    get_saeids_for_layer(sae_id_list:list, layer:int=0, width:int=16)
+
+    WORKS WITH GEMMA-SCOPE AND GPT2-SMALL RIGHT NOW
+    FOR GEMMA: assumes that there will be a layer_XX and a width_YY somewhere in the name
+    FOR GPT SMALL: assumes there will be blocks.XX. in the name
+    '''
+    # first tries gemmascope expectation
+    newlist= [s for s in sae_id_list if (f'width_{width}' in s and f'layer_{layer}' in s)]
+    # then tries gpt2small exepctation
+    if len(newlist) == 0:
+        newlist= [s for s in sae_id_list if (f'blocks.{layer}.' in s)]
+
+    return newlist
+
+def parse_sae_LO(sae_id):
+    '''
+    FOR GEMMASCOPE
+    assumes that there will be a _l0_X at the END OF THE NAME
+    '''
+    return int(sae_id.split('_l0_')[-1])
+
+def parse_sae_layer(sae_id):
+    '''
+    FOR GEMMASCOPE
+    assumes that there will be a 'layer_XX/' at the BEGINNING OF THE NAME
+    '''
+    return int(sae_id.split('/')[0].split('_')[-1])
+
+def lowest_L0_sae_id(sae_id_list:list):
+    '''
+    FOR GEMMASCOPE
+    assumes that there will be a _l0_X at the END OF THE NAME
+    '''
+    return min(sae_id_list, key=parse_sae_LO)
+
+
+def get_lowest_L0_sae_id_for_each_layer(model_df:DataFrame, layer=None, width=16):
+    '''
+    FOR GEMMASCOPE
+    This function takes DataFrame with a SINGLE ENTRY (single release) produced by sae_directory_info
+    e.g. model_df = sae_directory_info(model=model_name, release=release_id, exact_match_release=True)
+    Then it finds the lowest L0 for each layer of interest by name, not by dataframe
+    (WE SHOULD CHECK: tutorial 2.0 removes the L0 column, so I think it's the correc way?)
+    # layer should be a list of natural numbers or None
+    If Layer is None, returns lowest L0 sae_id for each of all layers'''
+
+    # THIS  - [0] - IS WHERE THE SINGLE DATAFRAME ENTRY COMES IN, CAN BE FIXED IN THE FUTURE
+    all_sae_ids = list(model_df.saes_map.to_list()[0].keys())
+
+    # layer is a list of integers
+    if layer is None:
+       layer = list(set([parse_sae_layer(s) for s in all_sae_ids]))
+
+    # initialize
+    nlayers = len(layer)
+    lowest_L0_sae_id_list = [None]*nlayers
+
+    for idx, ilayer in enumerate(layer):
+        layer_sae_ids = get_saeids_for_layer(all_sae_ids, layer=ilayer, width=width)
+        if len(layer_sae_ids) == 0:
+            print(f'No SAEs found for layer {ilayer}')
+            continue
+        lowest_L0_sae_id_list[idx] = lowest_L0_sae_id(layer_sae_ids)
+
+    return lowest_L0_sae_id_list
 
