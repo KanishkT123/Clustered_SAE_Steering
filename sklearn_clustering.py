2a04f92ae62f098a1b13ff116bfb0e98db001624
Sinem <38989094+sinemmy@users.noreply.github.com>
2024-09-13 11:36:26 -0400

    Update sklearn_clustering.py
    added and edited clustering functions (doesnt compute from sae but needs matrix as an input) from anger-calm notebook

--- sklearn_clustering.py
+++ sklearn_clustering.py
@@ -3,18 +3,74 @@
 # import libraries 
 import numpy as np
 
+
 from sklearn.preprocessing import StandardScalar
 from sklearn import metrics
-from sklearn.cluster import DBSCAN, HDBSCAN
+from sklearn.decomposition import PCA
 
-# for heirarchical clustering
-from sklearn.cluster import AgglomerativeClustering 
+
+# sklearn clustering methods
+from sklearn.cluster import DBSCAN, HDBSCAN, KMeans, SpectralClustering, AgglomerativeClustering
 
 # for scipy hierarchical clustering, linkage for computing, dendrogram for plotting
 from scipy.cluster.hierarchy import dendrogram, linkage 
 
-# X is the input data (features as numpy array)
+# umap
+import umap
 
+# plotting (only used for plotting dendrograms - set to False automatically, though this should be done separately)
+import matplotlib.pyplot as plt
+
+# decoder_matrix is the input data (features as numpy array)
+
+
+def run_kmeans_on_matrix(decoder_matrix:np.array, K=5000,random_state=0):
+  kmeans = KMeans(n_clusters=K, random_state=random_state)
+  kmeans.fit(decoder_matrix)
+  cluster_centers = kmeans.cluster_centers_
+  labels = kmeans.labels_
+  return cluster_centers, labels
+
+
+def run_pca_on_matrix (decoder_matrix, n_components):
+  pca = PCA(n_components=n_components)
+  decoder_matrix_2d = pca.fit_transform(decoder_matrix)
+  return decoder_matrix_2d
+
+= 'ward', \
+                     compute_distances: bool = False
+
+def run_agg_clustering(decoder_matrix, linkage='ward', n_clusters=None, \
+                       metric='eucledian',  distance_threshold=None, compute_distances=False):
+  '''
+  linkage can be any of: ['ward', 'complete', 'average', 'single']
+  '''
+  agg_clustering = AgglomerativeClustering(n_clusters=n_clusters, \
+                                            metric=metric, linkage=linkage, \
+                                            compute_distances=compute_distances, \
+                                            distance_threshold=distance_threshold)
+  clusters = agg_clustering.fit_predict(decoder_matrix)
+  return clusters
+
+
+# hierarchical clustering 
+def get_linkages (decoder_matrix, linkage='ward', plot_dendrogram = False):
+   Z = linkage(decoder_matrix, 'ward')
+   if plot_dendrogram:
+      plt.figure(figsize=(10, 7))
+      dendrogram(Z)
+      plt.show()
+   return Z
+      
+def run_spectral_clustering(decoder_matrix, n_clusters=10, affinity='nearest_neighbors', random_state=0):
+    spectral = SpectralClustering(n_clusters=n_clusters, affinity=affinity, random_state=random_state)
+    clusters = spectral.fit_predict(decoder_matrix)
+    return clusters
+
+def run_umap(decoder_matrix, n_components=2, random_state=0):
+  umap_model = umap.UMAP(n_components=n_components, random_state=random_state)
+  embedding = umap_model.fit_transform(decoder_matrix)
+  return embedding
 
 ##############################
 ## Density Based  Clustering #
@@ -23,37 +79,15 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 # DBSCAN : https://scikit-learn.org/stable/modules/clustering.html#dbscan
 # demo of DBSCAN: https://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html#sphx-glr-auto-examples-cluster-plot-dbscan-py
 
-def cluster_db (X:np.array, eps:float = 0.3, min_samples:int=10, standard_scale:bool=False):
+def run_dbscan (X:np.array, eps:float = 0.3, min_samples:int=10, standard_scale:bool=False):
     if standard_scale:
         X = StandardScaler().fit_transform(X)
     db = DBSCAN(eps=eps, min_samples=min_samples).fit(X)
     return db
 #  note: plot(X, hdb.labels_, hdb.probabilities_)
 
-        
 # HDBSCAN (should be preferred over DBSCAN)  
 # demo of HDBSCAN https://scikit-learn.org/stable/auto_examples/cluster/plot_hdbscan.html#sphx-glr-auto-examples-cluster-plot-hdbscan-py
-def cluster_hdb(X:np.array):
+def run_dbscan(X:np.array):
     hdb = HDBSCAN().fit(X)
     return hdb 
-#  note: plot(X, hdb.labels_, hdb.probabilities_)
-
-##############################
-## Hierarchical Clustering ##
-##############################
-# 1) Agglomerative Clustering
-##############################
-
-def cluster_agglo(X:np.array, n_clusters:int=2, \
-                   linkage: Literal['ward', 'complete', 'average', 'single']= 'ward', \
-                     compute_distances: bool = False):
-    
-    agg = AgglomerativeClustering(n_clusters=n_clusters, linkage=linkage, compute_distances=compute_distances).fit(X)
-    return agg
-
-
-##############################
-## Hierarchical Clustering ##
-##############################
-# 2) Divisive clustering
-##############################
